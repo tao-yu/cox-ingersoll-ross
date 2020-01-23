c8f441895503c1a6b43c0ff710dc0ff1ff535938
tao-yu <taoxyu@gmail.com>
2020-01-23 15:13:07 +0000

    replace mc_mean with mean in estimate order

--- sim/utils.py
+++ sim/utils.py
@@ -45,14 +45,14 @@ def brownian_paths(T, N, M):
     return t, W
 
 
-def estimate_order(scheme, T, n, M, ci_width=0.95):
+def estimate_order(scheme, T, n, M):
     t, W = brownian_paths(T, 2*n, M)
     t_n, X_n = scheme(t[::2], W[:,::2])
     t_2n, X_2n = scheme(t, W)
-    S_n = np.array(mc_mean(np.amax(np.abs(X_n - X_2n[:,::2]), axis=1), axis=0, ci_width=ci_width))
+    S_n = np.array(np.mean(np.amax(np.abs(X_n - X_2n[:,::2]), axis=1), axis=0))
 
     t_10, W_10 = brownian_paths(T, 20*n, M)
     t_10n, X_10n = scheme(t_10[::2], W_10[:,::2])
     t_20n, X_20n = scheme(t_10, W_10)
-    S_10n = np.array(mc_mean(np.amax(np.abs(X_10n - X_20n[:,::2]), axis=1), axis=0, ci_width=ci_width))
+    S_10n = np.array(np.mean(np.amax(np.abs(X_10n - X_20n[:,::2]), axis=1), axis=0))
     return np.log10(S_n) - np.log10(S_10n)
