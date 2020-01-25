a6de52524848c10ae6f422af5a40e2a0cd8c328b
tao-yu <taoxyu@gmail.com>
2020-01-25 02:24:33 +0000

    Add file for calculating alpha CIs

---
+++ sim/calculate_alpha.py
@@ -0,0 +1,52 @@
+import numpy as np
+from utils import *
+from solvers import *
+from tqdm import tqdm
+import scipy.stats as stats
+import time
+
+
+def timestamp():
+    return time.strftime("%m-%d_%H-%M")
+
+
+def convergence(scheme, k, lamda, T, X_0, n, M, f_range, num_alphas):
+    alphas = np.zeros((num_alphas, f_range.shape[0]))
+    for j in tqdm(range(num_alphas)):
+        for i, f in enumerate(tqdm(f_range)):
+            theta = np.sqrt(2*f)
+            alpha = estimate_order(k, lamda, theta, X_0, scheme, T, n, M)
+            alphas[j, i] = alpha
+    return alphas
+
+
+def record_convergence(scheme, k, lamda, T, X_0, n, M, f_range, num_alphas, name):
+    alphas = convergence(
+        k = k,
+        lamda = lamda,
+        T = T,
+        X_0 = X_0,
+        n = n,
+        M = M,
+        f_range = f_range,
+        scheme=explicit_scheme,
+        num_alphas=num_alphas
+    )
+    alphas = np.vstack([f_range, alphas])
+    np.savetxt(f"alphas/{name}_{timestamp()}_{k:.1f}_{lamda:.1f}_{T}_{X_0}_{n}_{M}.csv", 
+               alphas,
+               delimiter=",")
+
+            
+record_convergence(
+    k = 1,
+    lamda = 1,
+    T = 1,
+    X_0 = 1,
+    n = 200,
+    M = 3000,
+    f_range = np.concatenate([np.linspace(0.01, 0.15, 3), np.arange(0.2, 3.4, 0.2)]),
+    scheme=explicit_scheme,
+    num_alphas=30,
+    name="Ezero"
+)
