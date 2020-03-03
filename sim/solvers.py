5be01c01a1f48ea3676c80116409165e6cef539f
tao-yu <taoxyu@gmail.com>
2020-03-03 23:59:19 +0000

    Add adaptive heston path generator

--- sim/solvers.py
+++ sim/solvers.py
@@ -202,3 +202,79 @@ def hybrid_adaptive(k, lamda, theta, X_0, T, h_max, h_min, r):
 
     X = Y**2
     return t[:j+1], X[:j+1], backstop_usage[:j+1]
+
+
+@njit
+def adaptive_heston(k, lamda, theta, X_0, S_0, rf, cor, T, h_max, h_min, r):
+    alpha = (4*k*lamda - theta**2)/8
+    beta = -k/2
+    gamma = theta/2
+    
+    max_steps = int(T//h_min + 1)
+    t = np.zeros(max_steps)
+    Y = np.zeros(max_steps)
+    dWS = np.zeros(max_steps)
+    
+    backstop_usage = np.zeros(max_steps)
+    
+    Y[0] = np.sqrt(X_0)
+    Y_temp = Y[0]
+    
+    for j in range(1, max_steps):
+        h = h_max * np.minimum(1, np.abs(Y_temp)**r)
+        t_next = t[j-1] + h
+        
+        if h < h_min and t_next < T:
+            backstop_usage[j] = 1
+            t[j] = t[j-1] + h_min
+            W_inc = np.random.normal(0, np.sqrt(h_min))
+            dWS[j-1] = cor*W_inc + np.random.normal(0, np.sqrt(h_min)) * np.sqrt(1-cor**2)
+            discriminant = (Y_temp + gamma*W_inc)**2/(4*(1-beta*h)**2) \
+                    + (alpha*h)/(1-beta*h)
+            if discriminant < 0:
+                Y[j] = 0
+            else:
+                Y_temp = (Y_temp + gamma*W_inc)/(2*(1-beta*h)) \
+                         + np.sqrt(
+                             np.abs(discriminant)
+                         )
+                Y[j] = Y_temp
+
+        else:
+            if t_next > T:
+                h = T - t[j-1]
+                t_next = T
+            t[j] = t[j-1] + h
+            W_inc = np.random.normal(0, np.sqrt(h))
+            dWS[j-1] = cor*W_inc + np.random.normal(0, np.sqrt(h)) * np.sqrt(1-cor**2)
+            Y_temp = Y_temp + h*(alpha/Y_temp + beta*Y_temp) + gamma * W_inc
+            if Y_temp < 0:
+                backstop_usage[j] = 2
+                discriminant = (Y_temp + gamma*W_inc)**2/(4*(1-beta*h)**2) \
+                    + (alpha*h)/(1-beta*h)
+                if discriminant < 0:
+                    Y[j] = 0
+                else:
+                    Y_temp = (Y_temp + gamma*W_inc)/(2*(1-beta*h)) \
+                             + np.sqrt(
+                                 np.abs(discriminant)
+                             )
+                    Y[j] = Y_temp
+            else:
+                Y[j] = Y_temp
+        if t_next >= T:
+            break
+
+    X = Y**2
+    t, X, backstop_usage = t[:j+1], X[:j+1], backstop_usage[:j+1]
+    
+    S = np.zeros(X.shape)
+
+    S[0] = S_0
+    S_temp = S[0]
+    for j in range(1, X.shape[0]):
+        W_inc = dWS[j-1]
+        dt = t[j] - t[j-1]
+        S_temp = S_temp + rf*S_temp*dt + np.sqrt(X[j-1])*S_temp*W_inc
+        S[j] = S_temp
+    return S
